d73406b0ec2b6c62ae0050fe0f8dd17fcb529268
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2023-01-24 13:01:04 -0300

    feat: add pagination

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -3,11 +3,13 @@ package med.voll.api.medico;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
 @Table(name = "medicos")
 @Entity(name = "Medico")
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
